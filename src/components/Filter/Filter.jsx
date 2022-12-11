fd2efab147349d44ba29016b2256c95b301cd089
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-12-11 21:57:59 +0200

    fix filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { filterContacts } from 'redux/filterSlice';
 import { FilterInput } from './Filter.styled';
 
-const Filter = ({ onFilter }) => {
+const Filter = () => {
+  const dispatch = useDispatch();
+
   return (
     <>
       <label>
         Find contacts by name <br />
         <FilterInput
           type="text"
-          onChange={e => onFilter(e.currentTarget.value)}
+          onChange={e => dispatch(filterContacts(e.currentTarget.value))}
         />
       </label>
     </>
